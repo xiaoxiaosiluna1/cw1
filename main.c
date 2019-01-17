3c7322fbd23083daec16b369d3708c1f0b908aba
siluna <2261906492@qq.com>
2019-01-17 16:13:17 +0800

    添加车辆位置

--- main.c
+++ main.c
@@ -1,9 +1,43 @@
 #include <stdio.h>
-
+struct point 
+{
+    int x;
+    int y;
+};
 int main(){
 
-    printf("hello world!");
+     int index=0;
+    struct point arr[100];
+    printf("huanyingjinruzhuanchangweizhiguanlixitong!\n");
+    while(1)
+    {
+        printf("1---tianjiacheliangweizhi\n");
+        printf("2---shanchucheliangweizhi\n");
+        printf("3---xianshiquanbucheliangdeweizhi\n");
+        printf("4---fanhuiliwodeweizhizuijindecheliang\n");
+        printf("5---tuichu\n");
+        printf("qingxuanzexiangyingdegongneng\n");
+        int code;
+        scanf("%d",&code);
+        if(code==1)
+        {
+            printf("qingshurucheliangweizhidexzuobiaoheyzuobiao:\n");
+            int x;
+            int y;
+            scanf("%d", &x);
+            scanf("%d", &y);
 
+            arr[index].x = x;
+            arr[index].y = y;
+
+            index++;
+
+            printf("tianjiachenggong,dianjihuichejixu\n");
+            char xx;
+            scanf("%c",&xx);
+            scanf("%c",&xx);
+        }
+    }
     return 0;
-
+   
 }
